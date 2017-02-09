51eaf662337d042d3ba8ef0181b362ed6c7a9a3e
Valentin BATIOT <vbatiot@e2r8p10.42.fr>
2017-02-09 20:11:21 +0100

    asda

--- Js/script.js
+++ Js/script.js
@@ -55,54 +55,127 @@ function selectOnlyThis(id) {
 
 }
 
-function loadimage() {
-  var img = new Image();
-  img.onload 
+// CA COMMENCE ICI
+(function() {
+
+  var streaming = false,
+      video        = document.querySelector('#video'),
+      cover        = document.querySelector('#cover'),
+      canvas       = document.querySelector('#canvas'),
+      photo        = document.querySelector('#photo'),
+      startbutton  = document.querySelector('#startbutton'),
+      width = 320,
+      height = 0;
+
+  navigator.getMedia = ( navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia ||
+                         navigator.mozGetUserMedia ||
+                         navigator.msGetUserMedia);
+
+  navigator.getMedia(
+    {
+      video: true,
+      audio: false
+    },
+    function(stream) {
+      if (navigator.mozGetUserMedia) {
+        video.mozSrcObject = stream;
+      } else {
+        var vendorURL = window.URL || window.webkitURL;
+        video.src = vendorURL.createObjectURL(stream);
+      }
+      video.play();
+    },
+    function(err) {
+      console.log("An error occured! " + err);
+    }
+  );
+
+  video.addEventListener('canplay', function(ev){
+    if (!streaming) {
+      height = video.videoHeight / (video.videoWidth/width);
+      video.setAttribute('width', width);
+      video.setAttribute('height', height);
+      canvas.setAttribute('width', width);
+      canvas.setAttribute('height', height);
+      streaming = true;
+    }
+  }, false);
+
+  function loadImg(src) {
+    var ret = new Image();
+
+    ret.onload = onload = function(){ console.log("i'm here");};
+    ret.src = src;
+    return ret;
+  }
+
+
+function witchFilter()
+{
+  var cb1 = document.getElementById('cb1')
+  var cb2 = document.getElementById('cb2')
+  var cb3 = document.getElementById('cb3')
+
+  if (cb1.checked) {
+    var src = loadImg("../Filters/fokof.jpg");
+    console.log('cb1');
+  }
+  if (cb2.checked) {
+    var src = loadImg("../Filters/frog.png");  
+    console.log('cb2');
+  }
+  if (cb3.checked) {
+    var src = loadImg("../Filters/penguin.png");
+    console.log('cb3');
+  }
+  return src;
 }
 
-function takePicture() {
-  var img = witchOne();
-  if (img)
-  {
-    var ctx = canvas.getContext('2d');
-    ctx.drawImage(video, 0, 0);
-    ctx.globalAlpha = 1;
-    ctx.drawImage(img, 0,50);
-    var data = canvas.toDataURL('image/png');
-    canvas.setAttribute('src', data);
-    var xhr = getHttpRequest()
-    var post = new FormData()
-    post.append('img', data);
-    xhr.open('POST', 'http://localhost:8080/camagru/layer.php', true);
-    xhr.setRequestHeader('X-Requested-With', 'xmlhttprequest');
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-        //window.alert(xhr.responseText); // contient le résultat de la page
-      } else {
-        window.alert("wrong link");
+  function takepicture() {
+    var img = witchFilter();
+    if (img)
+    {
+      var ctx = canvas.getContext('2d');
+      ctx.drawImage(video, 0, 0, width, height);
+      ctx.globalAlpha = 1;
+      ctx.drawImage(img, 0, 50);
+      var data = canvas.toDataURL('image/png');
+      canvas.setAttribute('src', data);
+      var xhr = getHttpRequest()
+      var post = new FormData()
+      post.append('img', data);
+      xhr.open('POST', '../Controlers/layer.php', true);
+      xhr.setRequestHeader('X-Requested-With', 'xmlhttprequest');
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            //window.alert(xhr.responseText); // contient le résultat de la page
+          }
+          else {
+            window.alert("wrong link");
+          }
+        }
       }
+      xhr.send(post);
+    }
+    else {
+      alert('You must chose a filter');
     }
   }
-  xhr.send(post);
 
+  startbutton.addEventListener('click', function(ev){
+      takepicture();
+    ev.preventDefault();
+  }, false);
+
+  function radioCheck(id) {
+    for (var i = 1;i <= 3; i++) {
+        document.getElementById("cb" + i).checked = false;
+    }
+    document.getElementById(id).checked = true;
 }
 
-/*(function(){
-  var   filter_choice = document.querySelector('#choice'),
-      filter_prev = document.querySelector('#filter_prev')
+// ENLEVER LES FONCTIONS DOUBLE ET CHANGER LES IMAGES
 
-    console.log('1');
-    filter_choice.addEventListener('change', function() {
-      console.log('2');
-      var idx=filter_choice.selectedIndex;
-      var val=filter_choice.options[idx].value;
-   
-      filter_prev.classList.remove('filter1');
-      filter_prev.classList.remove('filter2');
-      filter_prev.classList.remove('filter3');
-
-      filter_prev.classList.add(val);
-
-    });
-})();*/
+})();
