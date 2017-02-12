a5ead15b5de7459b14c5496d335e2752854605a1
Valentin BATIOT <vbatiot@e1r9p11.42.fr>
2017-02-12 21:03:04 +0100

    asdA

--- Controlers/gallery.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-
-
-	$req = $db->prepare("SELECT * FROM gallery");
-	$req->execute();
-	$pic = $req->fetch();
-
-?>
--- Views/gallery.php
+++ Views/gallery.php
@@ -1,4 +1,16 @@
 <?php 
+
 	session_start();
+	require_once '../Config/setup.php';
+
+	$req = $db->prepare("SELECT * FROM gallery");
+	$req->execute();
+	$pic = $req->fetchAll();
+	$i = count($pic);
+
 	require '../Views/header.php';
- ?>
+	while (--$i >= 0) {
+		echo "<img id='".$i."' src='".$pic[$i]['link']."'/><br>";
+	}
+
+?>
